ab41f0d7eda5d684d70f5b35f95851a86a9a38bb
Ravi G Ravitharan <ravitharan@gmail.com>
2023-11-16 15:28:33 +0000

    Use shared memory for the interface

--- c/foo.c
+++ c/foo.c
@@ -1,21 +1,66 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+#define FILE_PATH_LEN       256
 
 struct parameter_t {
+  char shm_file[FILE_PATH_LEN];
   int count;
-  void *input;
-  void *output;
+  float multiplier;
 };
+
 
 int foo(struct parameter_t *param)
 {
-  int32_t *in_ptr = (int32_t *)param->input;
-  float *out_ptr = (float *)param->output;
-  int i, count = param->count;
+  uint8_t *shm_buf;
+  int32_t *in_data;
+  float *out_data;
+  int i, fd, ret = -1;
+  size_t pos, in_size, out_size, total_size;
+  int count = param->count;
+  float multiplier = param->multiplier;
 
-  for (i=0; i<count; i++) {
-    out_ptr[i] = 0.5 * in_ptr[i];
+  in_size = count * sizeof(*in_data);
+  out_size = count * sizeof(*out_data);
+  total_size = in_size + out_size;
+
+  fd = shm_open(param->shm_file, O_RDWR, 0);
+  if (fd == -1) {
+    perror("shm_open()");
+    goto exit;
   }
 
-  return 0;
+  /* Map the object into the caller's address space. */
+  shm_buf = mmap(NULL, total_size, PROT_READ | PROT_WRITE,
+                 MAP_SHARED, fd, 0);
+  if (shm_buf == MAP_FAILED) {
+    perror("mmap");
+    goto exit;
+  }
+
+  pos = 0;
+  in_data = (typeof(in_data))&shm_buf[pos];
+
+  pos += in_size;
+  out_data = (typeof(out_data))&shm_buf[pos];
+
+  for (i=0; i<count; i++) {
+    out_data[i] = multiplier * in_data[i];
+  }
+
+  munmap(shm_buf, total_size);
+
+  ret = 0;
+
+exit:
+  close(fd);
+
+  return ret;
 }
+
--- call_c.py
+++ call_c.py
@@ -1,13 +1,33 @@
 import ctypes
 import numpy as np
+from multiprocessing import shared_memory
 import pprint
+
+FILE_PATH_LEN = 256
+
+DATA_COUNT = 100
 
 class Parameter(ctypes.Structure):
     _fields_ = [
+        ("shm_file", ctypes.c_char * FILE_PATH_LEN),
         ("count", ctypes.c_int),
-        ("input", ctypes.c_void_p),
-        ("output", ctypes.c_void_p),
+        ("multiplier", ctypes.c_float),
     ]
+
+in_size     = DATA_COUNT * np.dtype('int32').itemsize
+out_size    = DATA_COUNT * np.dtype('float32').itemsize
+total_size  = in_size + out_size
+
+shm = shared_memory.SharedMemory(create=True, size=total_size)
+
+print(f'shm name {shm.name}')
+
+in_data     = np.frombuffer(shm.buf, dtype=np.int32, offset=0)
+out_data    = np.frombuffer(shm.buf, dtype=np.float32, offset=in_size)
+
+# Fill input data
+for i in range(len(in_data)):
+    in_data[i] = i
 
 lib = ctypes.cdll.LoadLibrary('./c/libfoo.so')
 
@@ -15,19 +35,25 @@ foo             = lib.foo
 foo.argtypes    = [ctypes.POINTER(Parameter)]
 foo.restype     = ctypes.c_int
 
-count = 100
-
-in_data = np.arange(count, dtype=np.int32)
-in_ptr = in_data.ctypes.data_as(ctypes.c_void_p)
-
-out_data = np.zeros(count, dtype=np.single)
-out_ptr = out_data.ctypes.data_as(ctypes.c_void_p)
-
-
-param = Parameter(count, in_ptr, out_ptr)
-
-a = foo(ctypes.byref(param))
-
+shm_name = ctypes.create_string_buffer(shm.name.encode(), FILE_PATH_LEN)
+param = Parameter(shm_name.raw, DATA_COUNT, 0.0)
+ret = foo(ctypes.byref(param))
+if (ret != 0):
+    print('Error in calling foo()')
+    exit(1)
 pprint.pprint(out_data)
+
+param.multiplier = 0.1
+ret = foo(ctypes.byref(param))
+pprint.pprint(out_data)
+
+param.multiplier = 0.5
+ret = foo(ctypes.byref(param))
+pprint.pprint(out_data)
+
+shm.close()
+shm.unlink()
+
+
 
 
